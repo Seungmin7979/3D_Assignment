355a0574e09eef7723d3d94dc641335daa3487a1
Seungmin7979 <safetmdals79@gmail.com>
2025-08-12 20:17:05 +0900

    [Feat] SSM : 점프대 설치 및 기능 추가

---
+++ Assets/Scripts/JumpPad/JumpingPad.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingPad : MonoBehaviour
+{
+    public float jumpForce = 15; // 점프대 힘
+
+    public void OnTriggerEnter(Collider other)
+    {
+        // 플레이어에 닿았는지 체크
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                // Y축 방향 속도를 0으로 초기화 후 위로 힘 가하기
+                rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            }
+        }
+    }
+}
